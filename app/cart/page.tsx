cfde5e732681ced184d65e8dbb9ca16397af1aec
MuhammedRiyasKT <145179614+MuhammedRiyasKT@users.noreply.github.com>
2025-08-28 15:53:32 +0530

    Update page.tsx

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -159,7 +159,7 @@ export default function CartPage() {
 
         console.log("User distance from hotel:", distance, "meters");
 
-        if (distance > 50) {
+        if (distance > 150) {
           alert("❌ You are outside the hotel. Orders are only allowed inside (within 50m).");
           return;
         }
